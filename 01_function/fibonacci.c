b5a4016398e985ddff6b165e1dfdb01989cad6d2
赵工 <4808844@qq.com>
2021-10-13 15:41:18 +0800

    优化斐波那契的迭代函数写法，更好理解。

--- 01_function/fibonacci.c
+++ 01_function/fibonacci.c
@@ -1,20 +1,17 @@
 /**
-* 用递归和迭代分别实现斐波那契，可以很明显看出递归耗资源和时间的特性
+ * 用递归和迭代分别实现斐波那契，可以很明显看出递归耗资源和时间的特性
+ * f(n) = f(n - 1) + f(n - 2)
 */
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
  * 递归实现
- * @param n
- * @return
  */
 unsigned int FibonacciRecursion(unsigned int n);
 
 /**
  * 迭代实现
- * @param n
- * @return
  */
 unsigned int FibonacciIteration(unsigned int n);
 
@@ -36,6 +33,9 @@ int main(void) {
   return EXIT_SUCCESS;
 }
 
+/**
+ * 递给形式代码可读性太好了！
+ */
 unsigned int FibonacciRecursion(unsigned int n) {
   if (n == 1 || n == 0) {
     return n;
@@ -43,16 +43,27 @@ unsigned int FibonacciRecursion(unsigned int n) {
   return FibonacciRecursion(n - 1) + FibonacciRecursion(n - 2);
 }
 
+/**
+ * 用迭代的方式来实现 f(n) = f(n - 1) + f(n - 2) 还是有些难理解的
+ */
 unsigned int FibonacciIteration(unsigned int n) {
-  unsigned int result = 0, result_prev = 0, result_prev_prev = 0;
+  if (n == 1 || n == 0) {
+    return n; // f(0) = 1, f(1) = 1
+  }
 
-  for (int i = 0; i < n; ++i) {
-    if (i == 0 || i == 1) {
-      result = i;
-    }
-    result_prev = result;
-    result += result_prev_prev;
-    result_prev_prev = result_prev;
+  // 其实只要看表达式的右边，牵涉到两个值，而不是 3 个值。
+  unsigned int last = 0;
+  unsigned int result = 1;
+
+  // 迭代的总体实现是控制循环次数
+  // 因为前两个数 0 和 1 的返回值是固定的，所以是少循环两次, 直接从 2 开始循环
+  // f(2) = f(1) + f(0) 接着
+  // f(3) = f(2) + f(1)
+  for (int i = 0; i <= n - 2; ++i) {
+    unsigned temp;
+    temp = result;
+    result += last;
+    last = temp;
   }
 
   return result;
