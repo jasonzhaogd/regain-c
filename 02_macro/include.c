f4605de64846536336f5dbca04403aea730f9891
赵工 <4808844@qq.com>
2021-10-13 18:12:20 +0800

    定义了标准输出的调试工具宏，并用 include 测试是否正常使用。唯一有问题的是 宽字符的打印输出还有问题，需要在独立的例子中测试。

---
+++ 02_macro/include.c
@@ -0,0 +1,33 @@
+/**
+ * 测试包含头文件，以及测试 io_utils 中的工具宏定义是否能正常使用
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#define PRINT_METADATA
+#include "../include/io_utils.h"
+
+int main(void) {
+  PRINT_CHAR('W');
+  PRINT_WCHAR(L'赵');
+  PRINT_INT(4);
+  PRINT_LONG(100L);
+  PRINT_LONGLONG(1000LL);
+  PRINT_HEX(0xFF);
+  PRINT_BOOL(-1);
+  PRINT_DOUBLE(3.14159267);
+  PRINT_STRING("Hello C");
+
+  int arr_int[] = {1, 2, 3, 4, 5};
+  // 注意，宏函数调用后面是否加分号，是不一定的，要看宏函数定义时最后是否已经加了分号或者需不需要。
+  PRINT_INT_ARRAY(arr_int, 5)
+  PRINT_INT_ARRAY_PER_LINE(arr_int, 5)
+
+  int arr_ch[] = {'j', 'a', 's', 'o', 'n'};
+  PRINT_CHAR_ARRAY(arr_ch, 10)
+
+  double arr_double[] = {1.0, 2.0, 3.0, 4.7};
+  PRINT_DOUBLE_ARRAY(arr_double, 10)
+
+  return EXIT_SUCCESS;
+}
---
+++ include/io_utils.h
@@ -0,0 +1,46 @@
+/**
+ * 标准输出的调试工具宏定义
+*/
+
+#ifndef REGAIN_C_INCLUDE_IO_UTILS_H_
+#define REGAIN_C_INCLUDE_IO_UTILS_H_
+
+#include <stdio.h>
+
+#ifdef PRINT_METADATA
+#define PRINT_LINE(format, ...) printf(__FILE__":%d [%s] - "format"\n", __LINE__, __FUNCTION__, ##__VA_ARGS__)
+#else
+#define PRINT_LINE(format, ...) printf(format"\n", ##__VA_ARGS__)
+#endif
+
+#define PRINT_CHAR(value) PRINT_LINE(#value" = %c", value)
+#define PRINT_WCHAR(value) PRINT_LINE(#value" = %lc", value)
+#define PRINT_INT(value) PRINT_LINE(#value" = %d", value)
+#define PRINT_LONG(value) PRINT_LINE(#value" = %ld", value)
+#define PRINT_LONGLONG(value) PRINT_LINE(#value" = %lld", value)
+#define PRINT_HEX(value) PRINT_LINE(#value" = %#x", value)
+#define PRINT_BOOL(value) PRINT_LINE(#value" = %s", value ? "true" : "false")
+#define PRINT_DOUBLE(value) PRINT_LINE(#value" = %g", value)
+#define PRINT_STRING(value) PRINT_LINE(#value" = %s", value)
+
+#define PRINT_ARRAY(format, array, length) \
+{                                          \
+  for(int i = 0; i < length; ++i) {        \
+    printf(format, array[i]);              \
+  };                                       \
+printf("\n");                              \
+}                                          \
+
+
+#define PRINT_INT_ARRAY_PER_LINE(array, length) \
+{                                                       \
+  for(int i = 0; i < length; ++i) {                     \
+    PRINT_LINE(#array"[%d] = %d", i, array[i]);         \
+  }                                                     \
+}
+
+#define PRINT_INT_ARRAY(array, length) PRINT_ARRAY("%d, ", array, length)
+#define PRINT_CHAR_ARRAY(array, length) PRINT_ARRAY("%c, ", array, length)
+#define PRINT_DOUBLE_ARRAY(array, length) PRINT_ARRAY("%lf, ", array, length)
+
+#endif // REGAIN_C_INCLUDE_IO_UTILS_H_
