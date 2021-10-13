674dd5f13320bc30fa79dd1c87cfa35dc8c2e647
赵工 <4808844@qq.com>
2021-10-13 11:12:14 +0800

    如何处理函数的变长参数

---
+++ 01_function/varargs.c
@@ -0,0 +1,32 @@
+/**
+ * 函数的边长参数，固定套路用法
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+void TestVarargs(int arg_count, ...) {
+  // 1. 定义 va_list 类型的变量，用于获取传入的变长参数
+  va_list args;
+
+  // 2. 开始遍历
+  va_start(args, arg_count);
+
+  for (int i = 0; i < arg_count; ++i) {
+    // 3. 依次取出对应参数， va_arg 的函数原型是 va_arg(va_list, type)
+    int arg = va_arg(args, int);
+    printf("第%d个参数是：%d\n", i+1, arg);
+  }
+
+  // 4. 结束遍历
+  va_end(args);
+}
+
+int main(void) {
+  TestVarargs(5, 11, 22, 33);
+  puts("");
+  TestVarargs(5, 11, 22, 33, 44, 55, 66, 77, 88, 99);
+  return EXIT_SUCCESS;
+}
+
